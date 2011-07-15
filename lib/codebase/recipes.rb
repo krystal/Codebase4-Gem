55cfe4b3a8ebebfe27df4b914d7477458b05d56a
Dan Wentworth <dan@atechmedia.com>
2011-07-15 14:04:22 +0100

    Cleaned up capistrano recipe

--- lib/codebase/recipes.rb
+++ lib/codebase/recipes.rb
@@ -1,38 +1,24 @@
-configuration = Capistrano::Configuration.respond_to?(:instance) ?
-  Capistrano::Configuration.instance(:must_exist) :
-  Capistrano.configuration(:must_exist)
+Capistrano::Configuration.instance(:must_exist).load do
 
-configuration.load do
-
-  after 'deploy:finalise', 'codebase:log_deployment'
+  after 'deploy:symlink', 'codebase:log_deployment'
 
   namespace :codebase do
     desc "Logs the deployment of your Codebase 4 repository"
     task :log_deployment do
 
-      ## get the revision deployed on the remote
-      old_revision, new_revision = nil
-      run "git --git-dir=#{deploy_to}/.git rev-list rollback -n 1" do |ch, st, data|
-        old_revision = data.chomp
-      end
-      
-      run "git --git-dir=#{deploy_to}/.git rev-list deploy -n 1" do |ch, st, data|
-        new_revision = data.chomp
-      end
-      
-      if old_revision == new_revision
+      if previous_revision == current_revision
         puts "\e[31m    The old revision & new revision are the same - you didn't deploy anything new. Skipping logging.\e[0m"
         next
       end
-      
-      cmd = ["cb deploy #{old_revision} #{new_revision}"]
+
+      cmd = ["cb deploy #{previous_revision} #{current_revision}"]
       
       cmd << "-s #{roles.values.collect{|r| r.servers}.flatten.collect{|s| s.host}.uniq.join(',') rescue ''}"
-      cmd << "-e #{fetch(:environment)}"
       cmd << "-b #{fetch(:branch)}"
+      #cmd << "-e #{fetch(:environment)}"
       
       ## get the repo and project name etc...
-      if fetch(:repository) =~ /git\@codebasehq.com\:(\w+)\/(\w+)\/(\w+)\.git\z/
+      if fetch(:repository) =~ /git\@codebasehq.com\:(.+)\/(.+)\/(.+)\.git\z/
         account, project, repo = $1, $2, $3
         cmd << "-r #{project}:#{repo}"
         cmd << "-h #{account}.codebasehq.com"
